64caa61b4cac182a48b85e27e93cd7a1b26b339e
digikar99 <shubhamkarayare@gmail.com>
2020-12-06 22:27:15 +0530

    [fix] results.jl can now run

--- GraphAttacks/src/results.jl
+++ GraphAttacks/src/results.jl
@@ -1,22 +1,22 @@
-using GraphAttacks
-using Plots
+using GraphAttacks, Plots, LightGraphs
 
 # can you check this? sclae free formulation seemsto be depcreceated
 # V=100
 # E=200
 # graph=scale_free(V,E)
 function cosine_sim(embeddings,dim_axis=1)
-	norm_embeddings=embeddings./sqrt.(sum((embeddings.*embeddings),dims=dim_axis))
-	transpose(norm_embeddings)*norm_embeddings
+  norm_embeddings=embeddings./sqrt.(sum((embeddings.*embeddings),dims=dim_axis))
+  transpose(norm_embeddings)*norm_embeddings
 end
+
 function predict_using_embeddings(train_graph::SimpleGraph,embeddings,
-                 per_node::Bool=PER_NODE)
+                                  per_node::Bool=PER_NODE)
 
     g = train_graph
 
     predictions  = nothing
     score_matrix = cosine_sim(embeddings)
-    
+
     if per_node
         predictions = Dict()
         # should "export JULIA_NUM_THREADS=n" in .bashrc to take advantage
@@ -44,24 +44,38 @@ end
 
 # g           = create_simple_graph("/home/shubhamkar/ram-disk/datasets/GRQ_test_0.net")
 g           = create_simple_graph("//home/chitrank/cs768_datasets/datasets/GRQ_test_0.net")
-budgets=[10 12 14 16 18 20]
+# budgets       = [10]
+budgets     = [10, 12, 14, 16, 18, 20]
 train, test = create_train_test_graph(g)
-train1=SimpleGraph(train)	
-pred_original        = predict(train, adamic_adar, per_node = true)
-acc_original=evaluate(train, test, pred, average_precision, per_node = true)
-ctr         = closed_triad_removal(train, test, budgets)
-acc_perturbed_AA=[]
-acc_perturbed_katz=[]
+train1      = SimpleGraph(train)
+pred_original = predict(train, adamic_adar, per_node = true)
+acc_original  = evaluate(train, test, pred_original, average_precision, per_node = true)
+ctr           = closed_triad_removal(train, test, budgets)
+println(ctr)
+
+acc_perturbed_AA   = []
+acc_perturbed_katz = []
 for perturbed_graph in ctr
-	pred    = predict(perturbed_graph, adamic_adar, per_node = true)
-	append!(acc_perturbed_AA,evaluate(perturbed_graph, test, pred, average_precision, per_node = true))
-	pred    = predict(perturbed_graph, katz, per_node = true)
-	append!(acc_perturbed_katz,evaluate(perturbed_graph, test, pred, average_precision, per_node = true))
-vals=zeros(length(budgets),2)
-vals[:,1]=acc_perturbed_AA
-vals[:,2]=acc_perturbed_katz
-p=plot(budgets,vals,label=["AA","katz"],title="Acc vs budgets")
+    global acc_perturbed_AA, acc_perturbed_katz
+    pred    = predict(perturbed_graph, adamic_adar, per_node = true)
+    push!(
+        acc_perturbed_AA,
+        evaluate(perturbed_graph, test, pred, average_precision, per_node = true)
+    )
+    pred    = predict(perturbed_graph, katz, per_node = true)
+    push!(
+        acc_perturbed_katz,
+        evaluate(perturbed_graph, test, pred, average_precision, per_node = true)
+    )
+end
+
+vals      = zeros(length(budgets),2)
+println(size(acc_perturbed_AA), " ", acc_perturbed_AA)
+vals[:,1] = acc_perturbed_AA
+vals[:,2] = acc_perturbed_katz
+p         = plot(budgets, vals, label = ["AA","katz"], title="Acc vs budgets")
 xlabel!(p,"budgets")
 ylabel!(p,"accuracy (MAP)")
 display(p)
+readline()
 
